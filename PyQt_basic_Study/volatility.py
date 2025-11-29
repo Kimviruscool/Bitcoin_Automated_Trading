7c5eba10bd348523cf7274f216ee1786b9f46cc5
Kimviruscool <sinsa9122@naver.com>
2025-11-29 21:50:13 +0900

    error code edit

--- PyQt_basic_Study/volatility.py
+++ PyQt_basic_Study/volatility.py
@@ -16,8 +16,8 @@ def get_target_price(ticker):
 def buy_crypto_currency(bithumb, ticker):
     krw = bithumb.get_balance(ticker)[2]
     orderbook = pybithumb.get_orderbook(ticker)
-    sell_price = orderbook['ask'][0]['price']
-    unit = krw/float(sell_price) * 0.7
+    sell_price = orderbook['asks'][0]['price']
+    unit = krw/float(sell_price) * 0.99
     return bithumb.buy_market_order(ticker, unit)
 
 def sell_crypto_currency(bithumb, ticker):
